98b81d1d4800fd5391332d26c113a58415c7dd73
egurapha <egurapha@gmail.com>
2020-01-26 23:56:38 -0800

    added card class

--- game/Card.py
+++ game/Card.py
@@ -1,5 +1,17 @@
 class Card:
     def __init__(self, suit, value):
+        self.suit = None
+        self.value = None
+        self.set_suit(suit)
+        self.set_value(value)
+
+    def __str__(self):
+        return self.suit + str(self.value)
+
+    def set_suit(self, suit):
+        assert suit in set(['S', 'H', 'C', 'D'])
         self.suit = suit
+
+    def set_value(self, value):
+        assert value in set(range(2, 14 + 1))
         self.value = value
-    
