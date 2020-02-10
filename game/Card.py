3439b0abb886a7f129662038615d721160e39d23
tcikezu <clark@clark-e5570.localdomain>
2020-02-09 23:22:19 -0800

    Wanted to fix some comments after applying python-pyling

--- game/Card.py
+++ game/Card.py
@@ -4,8 +4,16 @@ Card.py
 Card Module.
 """
 class Card:
-    """ Card class """
+    """ Card has public attributes suit and value, from which we form
+     card games.
+
+    Attributes:
+        suit (str): Representing (D)iamond, (C)lub, (H)eart, (S)pades.
+        value (int): Representing 2 through Ace (14).
+
+    """
     def __init__(self, suit, value):
+        """Initialize and create the card."""
         self.suit = None
         self.value = None
         self.set_suit(suit)
@@ -15,9 +23,11 @@ class Card:
         return self.suit + str(self.value)
 
     def set_suit(self, suit):
+        """ The suit of the card is set here. """
         assert suit in set(['S', 'H', 'C', 'D'])
         self.suit = suit
 
     def set_value(self, value):
+        """ Value of card set here. """
         assert value in set(range(2, 14 + 1))
         self.value = value
--- game/Deck.py
+++ game/Deck.py
@@ -6,58 +6,63 @@ Deck module.
 from Card import Card
 
 class Deck:
-    """ Deck class 
-    
-    Parameters
-    ------------
-    mode
-        A string to set the length of starting deck. This is either 'full' or
-        'small'.
+    """A Deck is an object that contains a collection of Cards.
+
+    Attributes:
+        mode (str): A string to set the length of starting deck. This is
+        either 'full' or 'small'.
+
     """
+
     def __init__(self, mode='full'):
+        """
+        Args:
+            mode (str): 'full' or 'small`
+
+        """
         self.mode = None
         self.deck = []
         self.set_mode(mode)
-        self.get_cards()
+        self._get_cards()
 
     def __str__(self):
-        """ Prints the entire deck.
-        """
+        """Prints the entire deck."""
         card_names = []
         for c in self.deck:
             card_names.append(c.__str__())
         return ' '.join(card_names)
 
     def __len__(self):
-        """ Returns the length of the current deck.
-        """
+        """ Returns the length of the current deck."""
         return len(self.deck)
 
     def set_mode(self, mode):
         """ Sets the mode of the deck.
 
         Args:
-            mode (str): 'full' or 'small'. Corresponds to playing with 52 cards or cards above 5 in value (of every suit).
+            mode (str): 'full' or 'small'. Corresponds to playing with
+            52 cards or cards above 5 in value (of every suit).
+
         """
         assert mode in set(['full', 'small'])
         self.mode = mode
 
     def build_deck(self, suits, values):
-        """ Method which adds Card objects to deck, to create a complete deck of our Deck object.
-        """
+        """ Method which adds Card objects to deck, to create a
+        complete deck of our Deck object."""
         for s in suits:
             for v in values:
-                self.deck.append(Card(s,v))
+                self.deck.append(Card(s, v))
 
     def _get_cards(self):
         """ Private method to build the deck given mode"""
         if self.mode == 'full':
             suits = ['S', 'C', 'H', 'D']
-            values = range(2,15)
+            values = range(2, 15)
 
         elif self.mode == 'small':
             suits = ['S', 'C', 'H', 'D']
-            values = range(6,15)
+            values = range(6, 15)
 
         else:
             raise Exception('Invalid Deck Type.')
